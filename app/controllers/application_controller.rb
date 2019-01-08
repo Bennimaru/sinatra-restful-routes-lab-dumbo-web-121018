aeab621cbf0a0d057827382cb88798ef461cc816
Bennimaru <bennywhyu@gamil.com>
2019-01-08 16:44:48 -0500

    first edits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,52 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get "/recipe/new" do
+    erb :new
+  end
+
+  post "/recipe" do
+    recipe= Recipe.new(params)
+    recipe.save
+    erb :create
+    redirect "/recipe/#{recipe.id}"
+  end
+
+  get "/recipe/:id" do
+    @recipe=Recipe.find(params[:id])
+    erb :show
+  end
+
+  get "/recipe/:id/edit" do
+    @recipe=Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch "/recipe/:id" do
+    @recipe=Recipe.find(params[:id])
+    @recipe.update(name:params[:name],ingredients:params[:ingredients],cook_time:params[:cook_time])
+    redirect "/recipe/#{recipe.id}"
+  end
+
+  get "/recipe" do
+    @recipe=Recipe.all
+    erb :index
+  end
+
+  get "/recipe/:id/delete" do
+    @recipe=Recipe.find(params[:id])
+    erb :delete
+  end
+
+  delete "/recipe/:id" do
+    @recipe=Recipe.find(params[:id])
+    @recipe.delete
+    redirect "/recipe"
+
+  end
+
+
+
+
 
 end
